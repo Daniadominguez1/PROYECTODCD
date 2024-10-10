af450f2b1cffb92681d1d34e6e01162d044fd33f
DANIA CORTES DOMINGUEZ <daniacortes324@gmail.com>
2024-10-10 10:11:07 -0600

    Se cambio un h1

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ if (!empty($_SESSION['activa'])) {
                 <img src="Cliente/img/ll.png" width="400px" height="400px" style="padding-top: 20px;">
             </div>
             <div class="col" style="background-color:#000497">
-                <h1 style="color: #ecf1ff">Autentificacion</h1>
+                <h1 style="color: #ecf1ff">Dania Cortés Domínguez</h1>
 
                 <form style="padding: 25px;" method="POST">
                     <div>
